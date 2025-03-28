1bcab0f4bf065e0e5e9f705cd141421efc6a4306
YeahmanS <yamansingh85@gmail.com>
2025-03-28 16:45:15 +0530

    added plenty of css, reset button, save pdf button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
+const doc = new jsPDF();
 
 const input = document.getElementById("user-input")
 const output = document.getElementById("render-area")
 const fonts = document.getElementById("font-select")
+const filename = document.getElementById("file-name")
+const savebutton = document.getElementById("save-button")
+const resetbutton = document.getElementById("reset-button")
+
+function saveRender() {
+    let pdfname = filename.value || "default"
+    doc.fromHTML(`<html><head><title>Mark Down Render</title><style>body{font-family :${fonts.value}}</style></head><body>` + output.innerHTML + `</body></html>`);
+    doc.save(`${pdfname}.pdf`);
+   }
 
 function render(){
     output.innerHTML=marked.parse(input.value)
@@ -11,5 +21,14 @@ function changeFont(){
     output.style.fontFamily = fonts.value
 }
 
+function clear(){
+    output.innerHTML=""
+    input.value=""
+    filename.value=""
+
+}
+
 input.addEventListener("input",render)
 fonts.addEventListener("change",changeFont)
+savebutton.addEventListener("click",saveRender)
+resetbutton.addEventListener("click",clear)
